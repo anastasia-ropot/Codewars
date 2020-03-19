29fe33be63b47eb94b545dc9098dffd8fc327d9c
Anastasia Ropot <nastyropottt999@gmail.com>
2020-03-19 22:11:27 +0300

    5: Fix the solution of Persistent Bugger task. Part 2

--- solutions/persistentBugger.js
+++ solutions/persistentBugger.js
@@ -1,20 +1,31 @@
-function getEvenNumberCount(num) {
-    if (num <= 1) {
-        return 0;
-    } else {
-        let number = num;
-        let count = 0;
-
-        if (number % 2 === 0) {
-            number = number / 2;
-            count = 1;
-        } else {
-            number = number - 3;
-            count = 0;
-        }
-        return getEvenNumberCount(number) + count;
-    }
+function persistence(num) {
+    return num < 10 ? 0 : persistence([...num.toString()].reduce((accumulator, currentValue) => accumulator * currentValue)) + 1;
 }
 
-console.log(getEvenNumberCount(3));
+console.log(persistence(999));
 
+function persistence1(num) {
+    let result = 0;
+
+    while(num > 9) {
+        result++;
+        num = [...num.toString()].reduce((accumulator, currentValue) => accumulator * currentValue);
+    }
+
+    return result;
+}
+
+console.log(persistence1(999));
+
+function persistence2(num) {
+    let array = [...num.toString()];
+    let count = 0;
+    while (array.length > 1) {
+        count++;
+        const result = array.reduce((accumulator, currentValue) => accumulator * currentValue);
+        array = [...result.toString()];
+    }
+    return count;
+}
+
+console.log(persistence2(999));
